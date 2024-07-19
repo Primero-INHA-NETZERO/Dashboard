42ca6a62343a59d61871724e1ce9a3fbb46215ac
lexxsh <john8538@naver.com>
2024-07-20 08:22:03 +0900

    빵추가

--- src/_mock/products.js
+++ src/_mock/products.js
@@ -4,45 +4,24 @@ import { faker } from '@faker-js/faker';
 // ----------------------------------------------------------------------
 
 const PRODUCT_NAME = [
-  'Nike Air Force 1 NDESTRUKT',
-  'Nike Space Hippie 04',
-  'Nike Air Zoom Pegasus 37 A.I.R. Chaz Bear',
-  'Nike Blazer Low 77 Vintage',
-  'Nike ZoomX SuperRep Surge',
-  'Zoom Freak 2',
-  'Nike Air Max Zephyr',
-  'Jordan Delta',
-  'Air Jordan XXXV PF',
-  'Nike Waffle Racer Crater',
-  'Kyrie 7 EP Sisterhood',
-  'Nike Air Zoom BB NXT',
-  'Nike Air Force 1 07 LX',
-  'Nike Air Force 1 Shadow SE',
-  'Nike Air Zoom Tempo NEXT%',
-  'Nike DBreak-Type',
-  'Nike Air Max Up',
-  'Nike Air Max 270 React ENG',
-  'NikeCourt Royale',
-  'Nike Air Zoom Pegasus 37 Premium',
-  'Nike Air Zoom SuperRep',
-  'NikeCourt Royale',
-  'Nike React Art3mis',
-  'Nike React Infinity Run Flyknit A.I.R. Chaz Bear',
+  '식빵',
+  '크로와상',
+  '바게트',
+  '시내롤',
+  '호두빵',
+  '크림빵',
+  '프레첼',
+  '마카롱',
+  '베이글',
+  '초코칩 쿠키',
+  '블루베리 머핀',
 ];
-const PRODUCT_COLOR = [
-  '#00AB55',
-  '#000000',
-  '#FFFFFF',
-  '#FFC0CB',
-  '#FF4842',
-  '#1890FF',
-  '#94D82D',
-  '#FFC107',
-];
+
+const PRODUCT_COLOR = [];
 
 // ----------------------------------------------------------------------
 
-export const products = [...Array(24)].map((_, index) => {
+export const products = [...Array(8)].map((_, index) => {
   const setIndex = index + 1;
 
   return {
